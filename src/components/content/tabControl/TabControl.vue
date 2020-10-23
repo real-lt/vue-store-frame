545ce645fa94e10643f42caa5fade71fbc61d83b
real_lt <ting_lv_ting@163.com>
2020-10-23 16:04:15 +0800

    tabcontrol组件封装和使用

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="tab-control">
+  <div v-for="(item, index) in titles" :key="index" class="tab-control-item" :class="{active: index===currentIndex}" @click="itemClick(index)">
+    <span>{{item}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  },
+}
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 16px;
+  height: 40px;
+  line-height: 40px;
+  background-color: #ffffff;
+}
+
+.tab-control-item {
+  flex: 1;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+
+.active span {
+  padding: 0 3px 3px;
+  border-bottom: 3px solid #da4c92;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,6 +6,18 @@
   <home-swiper :banners="banners" />
   <recommends-view :recommends="recommends" />
   <feature-view />
+  <tab-control class="tab-control" :titles="['title1', 'title2', 'title3']" />
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
+  <p>11</p>
   <p>11</p>
   <p>11</p>
   <p>11</p>
@@ -31,18 +43,22 @@
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar.vue";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendsView from "./childComps/RecommendsView.vue";
 import FeatureView from "./childComps/FeatureView.vue";
+
+import NavBar from "components/common/navbar/NavBar.vue";
+import TabControl from "components/content/tabControl/TabControl.vue";
+
 import { getHomeMultidata } from "network/home.js";
 export default {
   name: "Home",
   components: {
-    NavBar,
     HomeSwiper,
     RecommendsView,
-    FeatureView
+    FeatureView,
+    NavBar,
+    TabControl,
   },
   data() {
     return {
@@ -57,17 +73,13 @@ export default {
       this.banners = result.banners;
       this.recommends = result.recommends;
     }).catch((err) => {
-
+      console.error(err)
     });
   }
 }
 </script>
 
 <style scoped>
-#home {
-  /* padding-top: 44px; */
-}
-
 .home-nav {
   background-color: var(--color-tint);
   color: #ffffff;
@@ -77,5 +89,10 @@ export default {
   left: 0;
   right: 0;
   z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
